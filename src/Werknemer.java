d4a728b949d7fc1545ce04e097c8af8859bc6f84
Faissal070 <faisszal@hotmail.com>
2021-05-31 15:52:24 +0200

    implementatie en eerste lege test

---
+++ src/Werknemer.java
@@ -0,0 +1,7 @@
+public class Werknemer extends Werkkracht{
+
+    public Werknemer(String naam, Integer code, Integer telefoonnummer, Integer bsn) {
+        super(naam, code, telefoonnummer, bsn);
+    }
+}
+
